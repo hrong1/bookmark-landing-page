b6d9ce732b048e4cdb9b639cafd3eed7b04bbffb
hrong1 <hrong1@ucsc.edu>
2025-11-05 17:08:54 -0800

    mobile Ver completed

--- src/component/mainBody/finalContact/contact.tsx
+++ src/component/mainBody/finalContact/contact.tsx
@@ -1,5 +1,6 @@
 import './contact.scss'
 import { useState } from 'react';
+import Error from '../../../assets/icon-error.svg?react'
 
 function checkEmail(email: string) {
     const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -41,17 +42,23 @@ const Contact = () => {
             <h3 className='contact__title'>
                 Stay up-to-date with what we’re doing
             </h3>
-            <input 
-                className='contact__email' 
-                type="email" 
-                name="email"
-                autoComplete='email'
-                placeholder="Enter your email address"
-                aria-label="Email address for updates"
-                value={email}
-                onChange={handleChange}
-                onFocus={handleFocus}
-            />
+            <div className='contact__input'>
+                <input 
+                    className='contact__input--email' 
+                    type="email" 
+                    name="email"
+                    autoComplete='email'
+                    placeholder="Enter your email address"
+                    aria-label="Email address for updates"
+                    value={email}
+                    onChange={handleChange}
+                    onFocus={handleFocus}
+                />
+                <Error className='contact__input--error'/>
+                <small className="contact__input--error-text">
+                    Whoops, make sure it’s an email
+                </small>
+            </div>
             <button className='contact__submit' type='submit'>
                 Contact Us
             </button>
